5ad6ba8b06461ced05dd4e3066a49378c5b77f2f
Konstantin Dobler <konstantin.j.dobler@gmail.com>
2021-06-27 19:14:11 +0200

    improve template and klib

--- helpers/klib/misc.py
+++ helpers/klib/misc.py
@@ -27,9 +27,6 @@ import os
 from pytorch_lightning.loggers.wandb import WandbLogger
 from pytorch_lightning.utilities.distributed import rank_zero_only
 import wandb
-import torch
-import click
-import re
 
 
 class CustomWandbLogger(WandbLogger):
@@ -60,27 +57,3 @@ class kdict(dict):
     def __dir__(self):
         return self.keys()
 
-
-def process_click_args(ctx: click.Context, cmd_args: dict) -> int:
-    cmd_args = kdict(cmd_args)
-    if cmd_args.offline:
-        os.environ["WANDB_MODE"] = "dryrun"
-
-    ######### Handle CUDA & GPU stuff #########
-    num_gpus = len(cmd_args.gpus.split(',')
-                   ) if cmd_args.gpus else 0
-    if num_gpus > 0:
-        # Assume machine has not more than 100 GPUs
-        if not re.compile("^([0-9]|[1-9][0-9])(,([0-9]|[1-9][0-9]))*$").fullmatch(cmd_args.gpus):
-            ctx.fail(
-                f"invalid GPU string specified: \"{cmd_args.gpus}\". Expected format is a comma-seperated list of integers corresponding to GPU indices (execute nvidia-smi for more info)")
-        if not torch.cuda.is_available():
-            ctx.fail("GPUs were requested but machine has no CUDA!")
-        os.environ["CUDA_VISIBLE_DEVICES"] = cmd_args.gpus
-        torch.cudnn.enabled = True
-        torch.cudnn.benchmark = True
-    else:
-        os.environ["CUDA_VISIBLE_DEVICES"] = ""
-    cmd_args.gpus = num_gpus
-
-    return cmd_args
--- train.py
+++ train.py
@@ -1,4 +1,4 @@
-from helpers.klib import CustomWandbLogger, process_click_args
+import helpers.klib as klib
 import click
 import pytorch_lightning as pl
 from src.data_loading import BasicDataModule
@@ -19,7 +19,7 @@ WANDB_ENTITY = "<adjust me>"
 @click.option('--gpus', '-g', help="Specify in one string all the GPU indices like \"0,1,3,5\". Default is to use the CPU.")
 @click.option('--datasets', '-d', help="Datasets to train on", required=True, multiple=True, type=click.Path(exists=True, writable=True, file_okay=False))
 def main(ctx, **cmd_args):
-    cmd_args = process_click_args(ctx, cmd_args)
+    cmd_args = klib.click_helpers.process_click_args(ctx, cmd_args)
     manual_seed_specified = cmd_args.seed is not None
     cmd_args.seed  = pl.seed_everything(workers=True, seed=cmd_args.seed)
     print(cmd_args)
@@ -27,13 +27,14 @@ def main(ctx, **cmd_args):
     dm = BasicDataModule(
         data_dirs=cmd_args.datasets, workers=cmd_args.workers, batch_size=cmd_args.batch_size)
     model = BinaryClassifier()
-    wandb_logger = CustomWandbLogger(
+    wandb_logger = klib.misc.CustomWandbLogger(
         project=WANDB_PROJECT, entity=WANDB_ENTITY, job_type='train')
 
     # Initialize a trainer
     trainer = pl.Trainer(max_epochs=cmd_args.epochs,
                          progress_bar_refresh_rate=1,
                          gpus=cmd_args.gpus,
+                         accelerator=cmd_args.accelerator,
                          logger=wandb_logger,
                          benchmark=not manual_seed_specified,
                          deterministic=manual_seed_specified)
